a186c67e9ce3feacb8d1b05496f67aaf2a89efda
Inbarajasekaran <inbarajasekaran0011@gmail.com>
2021-07-23 16:16:44 +0530

    added register page

--- src/app/registeration/registeration.component.ts
+++ src/app/registeration/registeration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, RouterEvent } from '@angular/router';
 
 @Component({
   selector: 'app-registeration',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterationComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+  ) { }
+  registerForm: FormGroup;
+  loading = false;
+  submitted = false;
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      username: ['',[Validators.required]],
+      phone: ['', Validators.required, Validators.minLength(10), Validators.maxLength(10)],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(4)]],
+      retypePassword: ['', [Validators.required]]
+    });
   }
+
+
+  onFormSubmit() {
+    console.log("Working");
+    this.router.navigate['dashboard']
+  }
+
+  checkPassword(parameterName: FormGroup) { //ParameterName value should be given as FormGroup
+    return parameterName.get('password').value === parameterName.get('retypePassword').value;
+  }
+
+  // constructor( private fb: FormBuilder) { }
+  // registerForm: FormGroup
+
+  // ngOnInit(): void {
+  //   this.registerForm = this.fb.group({
+  //     name: ['', [Validators.required]],
+  //     username: ['', [Validators.required]],
+  //   })
+  // }
+
+  // submit(){
+  //   console.log("Working")
+  // }
 
 }
